405fcebf3cb408d52f1cc5bcc7d1377dbdf6faee
Nathi Sihlophe <nathi.sihlophe@gmail.com>
2025-12-15 21:21:17 +0200

    Add simple interest calculation script

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter principal amount:"
+read principal
+
+echo "Enter rate of interest (in %):"
+read rate
+
+echo "Enter time period (in years):"
+read time
+
+simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+
+echo "Simple Interest = $simple_interest"
